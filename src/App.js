37e30cdb8d0c383bd2e453d0dcb05adbf6ab5830
niranjan-sharma-s <niranjansharma1992@gmail.com>
2023-07-25 13:27:41 +0530

    feat added routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Link, Route, Routes, useParams } from "react-router-dom";
 
-function App() {
+const ImageList = () => {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+  const fetchImages = async () => {
+    try {
+      const response = await fetch("https://picsum.photos/v2/list");
+      const data = await response.json();
+      setImages(data);
+    } catch (error) {
+      console.error("Error fetching images:", error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Image Gallery</h1>
+      {images.map((image) => (
+        <Link key={image.id} to={`/image/${image.id}`}>
+          <img
+            key={image.id}
+            src={image.download_url}
+            alt='image'
+            style={{ width: "200px", height: "150px", margin: "10px" }}
+          />
+        </Link>
+      ))}
     </div>
   );
-}
+};
+
+const ImageDetails = () => {
+  const { id } = useParams();
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    fetchImageDetails();
+  }, []);
+
+  const fetchImageDetails = async () => {
+    try {
+      const response = await fetch(`https://picsum.photos/id/${id}/info`);
+      const data = await response.json();
+      setImage(data);
+    } catch (error) {
+      console.error("Error fetching image details:", error);
+    }
+  };
+
+  return (
+    <div>
+      {image ? (
+        <div>
+          <h2>Image Details</h2>
+          <img src={image.download_url} alt='image' style={{width:'300px' , height:'300px'}} />
+          <p>Author: {image.author}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<ImageList/>} />
+        <Route path="/image/:id" element={<ImageDetails/>} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
+
+
